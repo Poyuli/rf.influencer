ab7d598370901231c2b5b2c2a392cbfb53a2f6d6
Poyuli <exthrash@gmail.com>
2014-10-05 16:49:09 +0800

    Updated training file

--- train.py
+++ train.py
@@ -4,10 +4,12 @@ import math
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier as RFC
+from sklearn import svm, grid_search
 from sklearn.cross_validation import cross_val_score
 
 os.chdir("/Users/BradLi/Documents/Data Science/Kaggle/Influencer")
-writeFile = True
+writeFile = False
+model = "SVM"
 
 # Merging training and test sets
 df_train = pd.read_csv("train.csv", header = 0)
@@ -44,17 +46,32 @@ df['ratio_m'] = df.mrcv - df.msnt
 df['ft23'] = df.ft2 + df.ft3
 df = df.drop(['msnt','rsnt','posts','mrcv','rrcv','following','ft2','ft3'], axis=1)
 
+if model == "SVM":
+    df = (df - df.min()) / (df.max() - df.min())
+
 data = df.values
 
-# Random forest training
-forest = RFC(n_estimators = 100)
-cv_score = cross_val_score(forest, data[0:5500,1::], data[0:5500,0], cv=10)
-print "CV Score = ", cv_score.mean(),"\n"
-forest = forest.fit(data[0:5500,1::], data[0:5500,0])
-output = forest.predict_proba(data[5500::,1::])
-features = df.columns.tolist()[1:]
-print "Feature importances:"
-print zip(features, forest.feature_importances_)
+# Random forest or SVM training
+if model == "RF":
+    forest = RFC(n_estimators = 100)
+    cv_score = cross_val_score(forest, data[0:5500,1::], data[0:5500,0], cv=10)
+    print "CV Score = ", cv_score.mean(),"\n"
+    forest = forest.fit(data[0:5500,1::], data[0:5500,0])
+    output = forest.predict_proba(data[5500::,1::])
+    features = df.columns.tolist()[1:]
+    print "Feature importances:"
+    print zip(features, forest.feature_importances_)
+    
+elif model == "SVM":
+    svc = svm.SVC()
+    param = {'C':[1e3,1e2,1e1,1e0,1e-1,1e-2], 'gamma':[1e-1,1e0,1e1,1e2,1e3,1e4], 'kernel':['rbf']}
+    svc = grid_search.GridSearchCV(svc, param, cv=10)
+    svc.fit(data[0:5500,1::], data[0:5500,0])
+    output = svc.predict(data[5500::,1::])
+    print "Optimized parameters:"
+    print svc.best_estimator_
+    print "Best CV Score = "
+    print svc.best_score_
 
 # Writing predicted results to csv file
 if writeFile == True:
